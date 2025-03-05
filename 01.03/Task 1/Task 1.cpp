9c422f807ca34146d4fa39a604bc57fe9285f009
DDelamain <doroshenkodanila1@gmail.com>
2025-03-06 01:47:58 +0500

    Add files via upload

---
+++ 01.03/Task 1/Task 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int sec, mint, hour;
+int nsec, nmint, nhour;
+int msec, mmint, mhour;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите время в секундах: ";
+	cin >> sec;
+	sec %= 86400;
+	mint = sec / 60;
+	hour = mint / 60;
+	nsec = sec % 60;
+	nmint = mint % 60;
+	nhour = hour % 24;
+	cout << "\n\tВРЕМЯ СЕЙЧАС\n\n";
+	cout << "Часы: " << nhour << " Минуты: " << nmint << " Секунды: " << nsec;
+
+	cout << "\n\n\tДО ПОЛУНОЧИ ОСТАЛОСЬ\n";
+	mhour = ((86400 - sec) % 86400) / 3600;
+	mmint = (((86400 - sec) % 86400) % 3600) / 60;
+	msec = ((86400 - sec) % 86400) % 60;
+	cout << "\nЧасы: " << mhour << " Минуты: " << mmint << " Секунды: " << msec << endl;
+}
---
+++ 01.03/Task 2/Task 2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int sec,hour;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите время, прошедшее с начала рабочего дня, в секундах: ";
+	cin >> sec;
+	sec = 28800 - sec;
+	hour = abs(sec) / 3600;
+	cout << "Вам осталось работать: " << hour << " ч.";
+}
